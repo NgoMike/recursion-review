35c2287adf14e64ab6acec70568da204fa70453e
NgoMike <mqngo21@gmail.com>
2018-01-15 12:35:43 -0800

    Updated Src

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,24 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //check if Array
+  var result = [];
+
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return '' + obj;
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"'; 
+  } else if (Array.isArray(obj)) {
+    for (var i = 0; i < obj.length; i++) {
+      if (Array.isArray[obj[i]]) {
+        result.push(stringifyJSON(obj[i]));
+      }
+    }
+  } else if (obj instanceof Object) {
+  //objects
+    
+  }
+
+
+  return result;
 };
