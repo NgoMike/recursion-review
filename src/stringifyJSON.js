ba16f624899ad87b4c2b6341da46848b027b1b59
NgoMike <mqngo21@gmail.com>
2018-01-15 16:37:25 -0800

    Finished StringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -14,15 +14,34 @@ var stringifyJSON = function(obj) {
     return '"' + obj + '"'; 
   } else if (Array.isArray(obj)) {
     for (var i = 0; i < obj.length; i++) {
-      if (Array.isArray[obj[i]]) {
+      if (typeof obj[i] === 'string') {
+        result.push('"' + obj[i] + '"');
+      } else if (typeof obj[i] === 'number' || typeof obj[i] === 'boolean' || obj[i] === null) {
+        result.push(obj[i]);
+      }
+
+      if (obj[i] instanceof Object) {
         result.push(stringifyJSON(obj[i]));
       }
     }
+
+    return '[' + result + ']';
   } else if (obj instanceof Object) {
-  //objects
-    
+    var keys = Object.keys(obj);
+    var objStr = [];
+    for (var i = 0; i < keys.length; i++) {
+      var keyStr = '"' + keys[i] + '":'; 
+
+      if (obj[keys[i]] instanceof Function || obj[keys[i]] === undefined) {
+        continue;
+      } else if (typeof obj[keys[i]] === 'number' || typeof obj[keys[i]] === 'boolean' || obj[keys[i]] === null) {
+        objStr.push(keyStr + obj[keys[i]]);
+      } else {
+        objStr.push(keyStr + stringifyJSON(obj[keys[i]]));
+      }
+    }
+
+    objStr.join();
+    return '{' + objStr + '}';   
   }
-
-
-  return result;
 };
